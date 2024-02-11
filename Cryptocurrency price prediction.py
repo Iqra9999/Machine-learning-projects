3a0c7822174916ecad3c22a28ceb091930b9d6f7
Iqra Batool <149294792+Iqra9999@users.noreply.github.com>
2024-02-11 21:21:19 +0500

    Update Cryptocurrency price prediction.py

--- Cryptocurrency price prediction.py
+++ Cryptocurrency price prediction.py
@@ -1,3 +1,5 @@
+#Using the latest Bitcoin prices from Yahoo Finance using the yfinance API for this task.
+#importing necessary libraries
 import pandas as pd
 import yfinance as yf
 import datetime
@@ -18,9 +20,9 @@ data["Date"] = data.index
 data = data[["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"]]
 data.reset_index(drop=True, inplace=True)
 print(data.head())
-
 data.shape
 
+#visualizing the change in bitcoin prices till today by using a candlestick chart.
 import plotly.graph_objects as go
 figure = go.Figure(data=[go.Candlestick(x=data["Date"],
                                         open=data["Open"], 
@@ -31,9 +33,11 @@ figure.update_layout(title = "Bitcoin Price Analysis",
                      xaxis_rangeslider_visible=False)
 figure.show()
 
+#The Close column in the dataset contains the values we need to predict. So, let’s have a look at the correlation of all the columns in the data concerning the Close column.
 correlation = data.corr()
 print(correlation["Close"].sort_values(ascending=False))
 
+#using the AutoTS library to predict the bitcoin prices for the next 30 days.
 from autots import AutoTS
 model = AutoTS(forecast_length=30, frequency='infer', ensemble='simple')
 model = model.fit(data, date_col='Date', value_col='Close', id_col=None)
