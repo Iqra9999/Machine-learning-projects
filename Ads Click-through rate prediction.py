60f6a16aa58c036c868ea77e8a1c3d5eac124ea7
Iqra Batool <149294792+Iqra9999@users.noreply.github.com>
2024-02-11 21:16:01 +0500

    Update Ads Click-through rate prediction.py

--- Ads Click-through rate prediction.py
+++ Ads Click-through rate prediction.py
@@ -1,3 +1,5 @@
+#Dataset used: Click-through rate prediction (kaggle)
+#importing necessary python libraries
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
@@ -8,9 +10,11 @@ pio.templates.default = "plotly_white"
 data = pd.read_csv("ad_10000records.csv")
 print(data.head())
 
+#Transforming the "Clicked on Ad" column with 0 and 1 values: 0 represents 'no' (not clicked), and 1 represents 'yes' (clicked).
 data["Clicked on Ad"] = data["Clicked on Ad"].map({0: "No", 
                                1: "Yes"})
 
+#Click Through rate analysis based on the time spent by the users on website.
 fig = px.box(data, 
              x="Daily Time Spent on Site",  
              color="Clicked on Ad", 
@@ -20,6 +24,7 @@ fig = px.box(data,
 fig.update_traces(quartilemethod="exclusive")
 fig.show()
 
+#Click Through rate analysis based on the internet usage of the user.
 fig = px.box(data, 
              x="Daily Internet Usage",  
              color="Clicked on Ad", 
@@ -29,6 +34,7 @@ fig = px.box(data,
 fig.update_traces(quartilemethod="exclusive")
 fig.show()
 
+#Click through rate analysis based on the age of the user.
 fig = px.box(data, 
              x="Age",  
              color="Clicked on Ad", 
@@ -38,6 +44,7 @@ fig = px.box(data,
 fig.update_traces(quartilemethod="exclusive")
 fig.show()
 
+#Click through rate analysis based on the income of the user.
 fig = px.box(data, 
              x="Area Income",  
              color="Clicked on Ad", 
@@ -47,23 +54,26 @@ fig = px.box(data,
 fig.update_traces(quartilemethod="exclusive")
 fig.show()
 
+#Calculating the overall Ads click-through rate
 data["Clicked on Ad"].value_counts()
 
 click_through_rate = 4917 / 10000 * 100
 print(click_through_rate)
+#So 49.17 is the CTR.
 
+#Prediction Model
 data["Gender"] = data["Gender"].map({"Male": 1, 
                                "Female": 0})
 
 x=data.iloc[:,0:7]
 x=x.drop(['Ad Topic Line','City'],axis=1)
 y=data.iloc[:,9]
-
 from sklearn.model_selection import train_test_split
 xtrain,xtest,ytrain,ytest=train_test_split(x,y,
                                            test_size=0.2,
                                            random_state=4)
 
+#Training the model using Random forest classification algorithm
 from sklearn.ensemble import RandomForestClassifier
 model = RandomForestClassifier()
 model.fit(x, y)
@@ -71,6 +81,7 @@ model.fit(x, y)
 from sklearn.metrics import accuracy_score
 print(accuracy_score(ytest,y_pred))
 
+#Testing the model by making predictions.
 print("Ads Click Through Rate Prediction : ")
 a = float(input("Daily Time Spent on Site: "))
 b = float(input("Age: "))
